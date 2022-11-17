1b5fdcb4bb5a33a82f14a6970099995d8f6aa294
fortraie <kowalewskijakub@icloud.com>
2022-11-17 16:10:21 +0100

    Added first solution

--- src/JewelsAndStones771.java
+++ src/JewelsAndStones771.java
@@ -1,6 +1,30 @@
-public class JewelsAndStones771 {
-    public int numJewelsInStones(String jewels, String stones) {
-        return 0;
-    }
-}
+import java.util.HashSet;
 
+public class JewelsAndStones771 {
+
+    // You're given strings jewels representing the types of stones that are jewels, and stones representing the stones
+    // you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have
+    // are also jewels.
+    //
+    // Letters are case-sensitive, so "a" is considered a different type of stone from "A".
+
+    public int numJewelsInStones(String jewels, String stones) {
+
+        if (jewels == null || jewels.length() == 0) return 0;
+
+        int ctr = 0;
+        HashSet<Character> jewelSet = new HashSet<>();
+
+        for (char jewel : jewels.toCharArray()) {
+            jewelSet.add(jewel);
+        }
+
+        for (char stone : stones.toCharArray()) {
+            if (jewelSet.contains(stone)) ctr++;
+        }
+
+        return ctr;
+
+    }
+
+}
