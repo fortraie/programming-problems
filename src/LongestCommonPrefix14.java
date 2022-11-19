0f9984307f84dae71aec29ac76451a706915665b
fortraie <kowalewskijakub@icloud.com>
2022-11-19 13:46:03 +0100

    Add solution to LeetCode #14

---
+++ src/LongestCommonPrefix14.java
@@ -0,0 +1,36 @@
+public class LongestCommonPrefix14 {
+
+    /*
+
+    LeetCode #14 - Longest Common Prefix
+
+    Write a function to find the longest common prefix string amongst an array of strings.
+    If there is no common prefix, return an empty string "".
+
+     */
+
+    public static void main(String[] args) {
+        String [] strs = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+
+        if (strs == null || strs.length == 0) return "";
+
+        int minLength = strs[0].length();
+        for (String str : strs) if (minLength > str.length()) minLength = str.length();
+
+        StringBuilder sB = new StringBuilder();
+
+        for (int i = 0; i < minLength; i++) {
+            char tmp = strs[0].charAt(i);
+            for (String str : strs) {
+                if (str.charAt(i) != tmp) return sB.toString();
+            }
+            sB.append(tmp);
+        }
+
+        return sB.toString();
+    }
+}
